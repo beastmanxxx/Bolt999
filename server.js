64e59be0c463eb1579fec9dd8e32beb85040e3d5
Harshit Mogha <moghaeashu@gmail.com>
2025-09-06 09:21:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5555;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
+// Serve the main HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'landing.html'));
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}); 
